993c50f5c4a947f03c0007b725f8e2fcca28df66
arf111 <krsultan069@gmail.com>
2020-11-30 01:35:21 +0600

    Implemented Bird eye view

--- main.py
+++ main.py
@@ -49,79 +49,94 @@ def calc_dist(inp_vid, out_vid_path, yolo_v, conf_score):
         ret, frame = cap.read()
 
         if ret == False:
-            print("Frame not found")
             break
+        else:
+            # Get height and width of original frame.
+            (H, W) = frame.shape[0], frame.shape[1]
 
-        (H, W) = frame.shape[0], frame.shape[1]
+            # Set the ROI in the first frame of the input video.
+            if first_frame:
+                image = frame.copy()
 
-        # Set the ROI in the first frame of the input video.
-        if first_frame:
-            image = frame.copy()
+                while True:
+                    cv2.imshow("image", image)
+                    cv2.waitKey(1)
+                    if len(mouse_pts) == 7:
+                        cv2.destroyWindow("image")
+                        break
+            
+            # ordering => (bleft, tleft, tright, bright). The points must be drawn in this order.
+            src_points = np.float32(np.array(mouse_pts[:4]))
+            dest_points = np.float32([[0, H], [0, 0], [W, 0], [W, H]])
+            
+            # get the perspective matrix and warped image's height and width.
+            pers_matrix = cv2.getPerspectiveTransform(src_points, dest_points)
+            warped_img = cv2.warpPerspective(frame, pers_matrix, (W, H))
+            warped_imgH, warped_imgW = warped_img.shape[0], warped_img.shape[1] 
 
-            while True:
-                cv2.imshow("image", image)
-                cv2.waitKey(1)
-                if len(mouse_pts) == 7:
-                    cv2.destroyWindow("image")
-                    break
+            # bird eye view
+            bird_view = np.zeros((warped_imgH, warped_imgW, 3), np.uint8)
+            # bird eye view color background
+            bird_view[:] = (227, 240, 197)
+            
+            # User-defined points to get safe distance (approx 6 ft)
+            # perspectiveTransform expects 2D/3D channel of floating point array where each element is 2D/3D vector.
+            safe_points = np.float32(np.array([mouse_pts[4:6]]))
+            warped_safe_points = cv2.perspectiveTransform(
+                safe_points, pers_matrix)[0]
+
+            # From user-defined, we'll get the safe distance (approx 6 ft). Euclidean distance
+            safe_dist = np.sqrt((warped_safe_points[0][0] - warped_safe_points[1][0])**2 + (
+                warped_safe_points[0][1] - warped_safe_points[1][1]) ** 2)
+
+            # Draw the designated region in the image
+            cv2.polylines(frame, [np.array(mouse_pts[:4])], True, (0, 0, 0), 2)
+            
+            # --------------------------YOLOv5-------------------------#
+            
+            # Get the results from the model.
+            results = model(frame[:, :, ::-1])
+            
+            # detected bboxes with confidence score and class.
+            det_points = results.pred[0][:,:]
+            centroids = results.xywh[0][:,:2]
+            
+            # transform the centroids.
+            centroids_arr = get_Centroids_Lis(det_points, centroids, conf_score)
+            warped_centroids = cv2.perspectiveTransform(centroids_arr, pers_matrix)
+
+            # make a list of the warped centroids
+            warped_centroids_lis = list()
+            for i in range(warped_centroids.shape[0]):
+                warped_centroids_lis.append(
+                    [warped_centroids[i][0][0], warped_centroids[i][0][1]])
+            
+            # Show detection on main frame and draw line.
+            show_Detection(frame, bird_view, centroids_arr, warped_centroids_lis, det_points, warped_imgH, warped_imgW)
+            draw_RiskLine(frame, bird_view, safe_dist, centroids_arr, warped_centroids_lis, warped_imgH, warped_imgW)
+            
+            # Write to video and show image on window.
+            out.write(frame)
+            cv2.imshow('output', frame)
+            cv2.imshow('bird_view', bird_view)
+
+            # Write to bird view frame.
+            if first_frame:
+                fourcc2 = cv2.VideoWriter_fourcc(*'XVID')
+                out2 = cv2.VideoWriter(out_vid_path+"/bird_view.avi",
+                          fourcc, inp_fps,  (bird_view.shape[1], bird_view.shape[0]))
+            else:
+                out2.write(bird_view)
 
             first_frame = False
-
-        # ordering => (bleft,tleft,tright,bright)
-        # The points must be drawn in this order.
-        src_points = np.float32(np.array(mouse_pts[:4]))
-        dest_points = np.float32([[0, H], [0, 0], [W, 0], [W, H]])
-        
-        pers_matrix = cv2.getPerspectiveTransform(src_points, dest_points)
-        warped_img = cv2.warpPerspective(frame, pers_matrix, (W, H))
-        warped_imgH, warped_imgW = warped_img.shape[0], warped_img.shape[1] 
-
-        # User-defined points to get safe distance (approx 6 ft)
-        # perspectiveTransform expects 2D/3D channel of floating point array where each element is 2D/3D vector.
-        safe_points = np.float32(np.array([mouse_pts[4:6]]))
-        warped_safe_points = cv2.perspectiveTransform(
-            safe_points, pers_matrix)[0]
-
-        # From user-defined, we'll get the safe distance (approx 6 ft). Euclidean distance
-        safe_dist = np.sqrt((warped_safe_points[0][0] - warped_safe_points[1][0])**2 + (
-            warped_safe_points[0][1] - warped_safe_points[1][1]) ** 2)
-
-        # Draw the designated region in the image
-        cv2.polylines(frame, [np.array(mouse_pts[:4])], True, (0, 0, 0), 2)
-        
-        # --------------------------YOLOv5-------------------------#
-        # Get the centroids and bbox visualizer
-
-        results = model(frame[:, :, ::-1])
-        
-        det_points = results.pred[0][:,:] # detected bboxes with confidence score and class.
-        centroids = results.xywh[0][:,:2]
-        
-        centroids_arr = get_Centroids_Lis(det_points, centroids, conf_score)
-        warped_centroids = cv2.perspectiveTransform(centroids_arr, pers_matrix)
-        
-        warped_centroids_lis = list()
-
-        for i in range(warped_centroids.shape[0]):
-            warped_centroids_lis.append(
-                [warped_centroids[i][0][0], warped_centroids[i][0][1]])
-        
-        # Show detection on main frame and draw line.
-        draw_RiskLine(frame, safe_dist, centroids_arr, warped_centroids_lis, warped_imgH, warped_imgW)
-        show_Detection(frame, centroids_arr, warped_centroids_lis, det_points, warped_imgH, warped_imgW)
-        
-        out.write(frame)
-        
-        cv2.imshow('output', frame)
-        # Press 'q' for exit.
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Press 'q' for exit.
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     # Release everything if job is finished
     cap.release()
     out.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     # Receives arguments specified by user
--- utils/safedist_utils.py
+++ utils/safedist_utils.py
@@ -25,7 +25,7 @@ def get_Centroids_Lis(points, centroids, conf_score):
 
     return np.float32(np.array(centroids_lis)).reshape(-1, 1, 2)
 
-def draw_RiskLine(img, minm_dist, centroids_arr, transformed_centroids, warped_imgH, warped_imgW):
+def draw_RiskLine(img, bird_view, minm_dist, centroids_arr, transformed_centroids, warped_imgH, warped_imgW):
     """Find out the violation of social distance measure and return the frame with those points 
     Arguments:
     @ img: frame
@@ -39,13 +39,15 @@ def draw_RiskLine(img, minm_dist, centroids_arr, transformed_centroids, warped_i
                 if i!=j:
                     pair = [[transformed_centroids[i][0], transformed_centroids[i][1]], 
                         [transformed_centroids[j][0], transformed_centroids[j][1]]]
-                    if checkIfReg(pair, warped_imgW, warped_imgH):
+                    if checkIfReg(pair, warped_imgW, warped_imgH): 
                     # Check if the distance between each combination of points is less than the minimum distance chosen
                         if int(math.sqrt( (pair[0][0] - pair[1][0])**2 + (pair[0][1] - pair[1][1])**2 )) < int(minm_dist): 
                             img = cv2.line(img, (int(centroids_arr[i][0][0]), int(centroids_arr[i][0][1])), 
                                     (int(centroids_arr[j][0][0]), int(centroids_arr[j][0][1])), color=(0,0,255), thickness=2) # red line
+                            bird_view = cv2.line(bird_view, (int(pair[0][0]), int(pair[0][1])), 
+                                    (int(pair[1][0]), int(pair[1][1])), color=(0,0,255), thickness=2)
     
-def show_Detection(img, centroids_arr, transformed_centroids, det_points, warped_imgH, warped_imgW):
+def show_Detection(img, bird_view, centroids_arr, transformed_centroids, det_points, warped_imgH, warped_imgW):
     """Draw the detected boxes and centroids inside the designated region.
     Arguments:
     @ img: frame
@@ -60,7 +62,10 @@ def show_Detection(img, centroids_arr, transformed_centroids, det_points, warped
         for i in range(len(transformed_centroids)):
             # check if transformed_centroids are in the designated region
             if not ((transformed_centroids[i][0] > warped_imgW or transformed_centroids[i][0] < 0) or (transformed_centroids[i][1] > warped_imgH or transformed_centroids[i][1] < 0)):
-                # Draw BBoxes, centroids and write down class_name.
+                # Define the detections as circle in bird view image.
+                bird_view = cv2.circle(bird_view, (int(transformed_centroids[i][0]), int(transformed_centroids[i][1])), 3, (0,255,0), 3)
+                
+                # Draw the Bboxes, centroids, and write down class name.
                 img = cv2.rectangle(img,(int(det_points[i][0]), int(det_points[i][1])),
                     (int(det_points[i][2]), int(det_points[i][3])), (140, 102, 39), 2)
                 img = cv2.circle(img, (int(centroids_arr[i][0][0]), int(centroids_arr[i][0][1])), 3, (0,255,0), 3)
